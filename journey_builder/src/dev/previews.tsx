4e6599d756ec4fdb323e90cee39ffbca81920387
Mili Bovan <milibovan190d@gmail.com>
2025-11-26 12:23:34 +0100

    clean: Cleaned previews component

--- journey_builder/src/dev/previews.tsx
+++ journey_builder/src/dev/previews.tsx
@@ -1,16 +1,12 @@
 import {ComponentPreview, Previews} from "@react-buddy/ide-toolbox";
 import {PaletteTree} from "./palette";
 import App from "../App.tsx";
-import CustomFlow from "../components/CustomFlow.tsx";
 
 const ComponentPreviews = () => {
     return (
         <Previews palette={<PaletteTree/>}>
             <ComponentPreview path="/App">
                 <App/>
-            </ComponentPreview>
-            <ComponentPreview path="/Flow">
-                <CustomFlow/>
             </ComponentPreview>
         </Previews>
     );
